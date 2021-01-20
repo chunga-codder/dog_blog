4188d751d5dc0c87027c57679cf9b0a2725b1cbb
Franco <phanfranco@yahoo.com>
2021-01-19 21:24:02 -0800

    Fixed routing issue.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Use Express
 const express = require("express");
-const app = express();  // don't need to require pug (built into Express somehow?);
-
+const app = express(); // don't need to require pug (built into Express somehow?);
+// // Requiring our models for syncing
+const db = require("./models");
 // Set Port
 const PORT = process.env.PORT || 8080;
 
@@ -9,24 +10,18 @@ const PORT = process.env.PORT || 8080;
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-// Import routes and give the server access to them.
-const routes = require('./routes/api-routes.js');
-app.use(routes);
-
-
-// // Requiring our models for syncing
-const db = require("./models");
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Import routes and give the server access to them.
+const routes = require("./routes/api-routes.js");
+app.use("/", routes);
+
 // Static directory
 app.use(express.static("public"));
 
-
-
 // // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+  app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
 });
