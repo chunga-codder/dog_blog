3c9a1080324b1afe6dea6e8928962b77fd89b535
Jayrich <jrkrichards@gmail.com>
2021-01-21 07:29:56 -0800

    UPDATE: created a html routes to render the html pages

---
+++ routes/html-routes.js
@@ -0,0 +1,39 @@
+// Requiring path to so we can use relative routes to our HTML files
+const express = require("express");
+const router = express.Router();
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/authenticated");
+
+// Rendering home page on launch
+router.get("/", (req, res) => {
+    res.render("pug");
+});
+
+// Rendering sign-in page
+router.get("/signin", function(req, res) {
+// If the user already has an account send them to the members page
+if (req.user) {
+    res.redirect("/members");
+}
+// res.sendFile(path.join(__dirname, "../public/signup.html"));
+res.render("accounts");
+});
+
+// NOT DONE
+// router.get("/login", function(req, res) {
+// // If the user already has an account send them to the members page
+// if (req.user) {
+//     res.redirect("/members");
+// }
+// res.sendFile(path.join(__dirname, "../public/login.html"));
+// });
+
+// // NOT DONE
+// // Here we've add our isAuthenticated middleware to this route.
+// // If a user who is not logged in tries to access this route they will be redirected to the signup page
+// router.get("/members", isAuthenticated, function(req, res) {
+// res.sendFile(path.join(__dirname, "../public/members.html"));
+// });
+
+module.exports = router;
