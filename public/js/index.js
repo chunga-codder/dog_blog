ef0e3ef077fed7626b933166a4927f9eeca4b348
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-01-23 12:23:05 -0800

    added blockblog, added submit and newpost function, able to pull values

--- public/js/index.js
+++ public/js/index.js
@@ -15,3 +15,56 @@ $(document).ready(function () {
     $("#parks-section").show();
   });
 });
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("DOM loaded! 🚀");
+
+  const usernameInput = document.getElementById("usernameInput");
+  const titleInput = document.getElementById("titleInput");
+  const bodyInput = document.getElementById("bodyInput");
+
+  const createPost = (e) => {
+    e.preventDefault();
+    if (!titleInput.value || !bodyInput.value) {
+      alert("Your post is missing some content");
+    }
+
+    const newPost = {
+      //   username: usernameInput.value.trim(),
+      title: titleInput.value.trin(),
+      body: bodyInput.value.trim(),
+      // category: postCategorySelect.value,
+    };
+    console.log("createPost -> newPost", newPost);
+    // Check if the user is updating or creating and preform said function
+    //   if (updating) {
+    //     newPost.id = postId;
+    //     updatePost(newPost);
+    //   } else {
+    submitPost(newPost);
+    //   }
+  };
+
+  const submitPost = (post) => {
+    fetch("/api/blog", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(post),
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        console.log("Success");
+        location.reload();
+        console.log(post);
+      })
+      .catch((err) => {
+        console.error("Error");
+      });
+  };
+
+  const formBtn = document.querySelector("#form-btn");
+
+  formBtn.addEventListener("click", createPost);
+});
