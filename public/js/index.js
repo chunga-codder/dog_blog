1e659a5b3997738802682320b41f0d1a273583e4
Jayrich <jrkrichards@gmail.com>
2021-01-25 21:05:52 -0800

    UPDATE index.js: updated the overall js code to show the login and logout information

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,17 @@
 $(document).ready(() => {
     console.log("in index.js")
+    $.get("/api/user_data").then(function(data) {
+        console.log(data)
+        if(data.email) {
+            $('#navLogout').removeClass('hide')
+        } else {
+            $('#navLogin').removeClass("hide")
+        };        
+    });
+    $('#navLogin').on('click', () => {
+        window.location.replace("/signin");
+    });
+    $('#navLogin').on('click', () => {
+        window.location.replace("/logout");
+    });
 }); 
